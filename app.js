05dd64da054386bcb68e1ac97a75880577b00f32
Barlium <mathias.bauer@live.de>
2021-03-04 00:11:02 +0100

    ws and Websocketio doesnt work on this production server. On other production servers, ws and Websocketio does work.

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-/*const express = require('express'); // Express as webserver
+const express = require('express'); // Express as webserver
 const socketIO = require("socket.io");
 const PORT = process.env.PORT || 3000;
 
@@ -78,25 +78,3 @@ io.on('connection', (socket) => {
     console.log("Client connected");
     socket.on('disconnect', () => console.log('Client disconnected'));
 });
-*/
-
-'use strict';
-
-const express = require('express');
-const socketIO = require('socket.io');
-
-const PORT = process.env.PORT || 3000;
-const INDEX = 'public/html/index.html';
-
-const server = express()
-    .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
-    .listen(PORT, () => console.log(`Listening on ${PORT}`));
-
-const io = socketIO(server);
-
-io.on('connection', (socket) => {
-    console.log('Client connected');
-    socket.on('disconnect', () => console.log('Client disconnected'));
-});
-
-setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
