cc676cda04f4cfabaa3035be0d91b8d6e2f031b1
Barlium <mathias.bauer@live.de>
2021-03-04 00:06:39 +0100

    Another Websocket test...

--- app.js
+++ app.js
@@ -86,7 +86,7 @@ const express = require('express');
 const socketIO = require('socket.io');
 
 const PORT = process.env.PORT || 3000;
-const INDEX = '/index.html';
+const INDEX = 'public/html/index.html';
 
 const server = express()
     .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
