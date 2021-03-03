9797e241bcefd8d4ff94ac650974266d7e33c89b
Barlium <mathias.bauer@live.de>
2021-03-03 23:05:54 +0100

    no message

--- app.js
+++ app.js
@@ -84,17 +84,11 @@ const server = express()
     .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
     .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-const { Server } = require('ws');
+const io = socketIO(server);
 
-const wss = new Server({ server });
-
-wss.on('connection', (ws) => {
+io.on('connection', (socket) => {
     console.log('Client connected');
-    ws.on('close', () => console.log('Client disconnected'));
+    socket.on('disconnect', () => console.log('Client disconnected'));
 });
 
-setInterval(() => {
-    wss.clients.forEach((client) => {
-        client.send(new Date().toTimeString());
-    });
-}, 1000);
+setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
