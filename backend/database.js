7f31070978dfdd84ce3858de5319365e5cd4c94f
Andre Helgert <GitlabAndre>
2021-03-12 09:32:01 +0100

    LoadRooms Test

--- backend/database.js
+++ backend/database.js
@@ -39,7 +39,7 @@ function loadRooms(moodleroomid) {
         }
     });
 
-client.query(`SELECT * FROM rooms WHERE moodleroomid = moodleroomid;`, (err, res) => {
+client.query(`SELECT * FROM rooms WHERE moodleroomid = moodleRoom;`, (err, res) => {
     if (err) {
         console.log("Error - Konnte nicht Moodle Raum ID 1970 ziehen!");
         console.log(err);
