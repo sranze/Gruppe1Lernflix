bcec8a3d1c1fca8f383991853b31a313061c7bc9
Andre Helgert <GitlabAndre>
2021-04-08 13:10:52 +0200

    Updated Front end stuff from okan with functionality components from mathias. added and changed some visual stuff.

--- app.js
+++ app.js
@@ -243,10 +243,10 @@ io.on('connection', (socket) => {
         socket.on('addFlag', flagInformation => {
             const user = getCurrentUser(socket.id)
             if (typeof user !== 'undefined') {
-                if (flagInformation !== 'undefined') {
-                    console.log("Received Flag information: RoomID: " + flagInformation.roomID + " Current Time: " + flagInformation.videoTime + " Video URL: " + flagInformation.videoURL + " Annotation: " + flagInformation.annotation);
+                //if (flagInformation !== 'undefined') {
+                   // console.log("Received Flag information: RoomID: " + flagInformation.roomID + " Current Time: " + flagInformation.videoTime + " Video URL: " + flagInformation.videoURL + " Annotation: " + flagInformation.annotation);
                     saveFlag(flagInformation);
-                }
+               // }
                 // Load flags
                 const flags = loadFlags(user.roomId, flagInformation.videoURL);
                 io.to(user.roomId).emit('updateFlags', flags);
