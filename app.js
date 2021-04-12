8f680f2b715fa32638ee5cbf450c35e619a6726c
Andre Helgert <GitlabAndre>
2021-04-12 15:58:14 +0200

    Versuch von .env

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 require('dotenv').config()
 
-const Filter = require('bad-words'),
-
-
 const express = require('express'); // Express as webserver
+const Filter = require('bad-words'),
 const PORT = process.env.PORT;
 const socketIO = require('socket.io');
 const path = require('path');
