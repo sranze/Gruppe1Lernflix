ba9ac51c587ecc679cb9011835a2af6d6ac84ed7
Andre Helgert <GitlabAndre>
2021-04-14 13:52:24 +0200

    Feedback Button DB

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const socketIO = require('socket.io');
 const path = require('path');
 const { messageFormatter, welcomeMessage } = require('./backend/messages'); // make messages.js available
 const { userJoin, getCurrentUser, userLeave } = require('./backend/users'); // make functions in users.js available
-const { saveUser, loadRooms, saveRooms } = require('./backend/database'); // make database functions available
+const { saveUser, loadRooms, saveRooms, saveFeedback } = require('./backend/database'); // make database functions available
 const { loadVideoInformation, saveVideoInformation } = require('./backend/videos'); // make room (video-information) functions available
 const { loadFlags, saveFlag, removeFlag } = require('./backend/flags') // make flag functionalities available
 
