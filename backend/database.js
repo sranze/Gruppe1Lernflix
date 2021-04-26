c5aa146b784ebccbba7af53260f4198b647c2b5c
Andre Helgert <GitlabAndre>
2021-04-26 09:58:03 +0200

    Feedback Button

--- backend/database.js
+++ backend/database.js
@@ -41,7 +41,7 @@ function saveFeedback( userid, username, feedbackText, moodleRoom, moodleRoomNam
             var timestamp = new Date();
             const client = newPool();
 
-            client.query(`INSERT INTO feedbackLernflix( userid, username, feedbackText, moodleRoom, moodleRoomName, timestamp) SELECT $1, $2, $3, $4, $5, $6`, [ userid, username, feedbackText, moodleRoom, moodleRoomName, timestamp], (err, res) => {
+            client.query(`INSERT INTO "feedbackLernflix"( userid, username, feedbackText, moodleRoom, moodleRoomName, timestamp) SELECT $1, $2, $3, $4, $5, $6`, [ userid, username, feedbackText, moodleRoom, moodleRoomName, timestamp], (err, res) => {
                 if (err) {
                     console.log("Feedback geht nicht SCHON VORHANDEN!");
                     console.log(err);
