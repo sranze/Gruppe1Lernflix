fea1a77d84c50987fb754e767269c9f97899f1d2
Andre Helgert <GitlabAndre>
2021-06-01 10:27:44 +0200

    test

--- backend/database.js
+++ backend/database.js
@@ -26,7 +26,7 @@ function saveUser(firstname, lastname, fullname, email, userid, profilepicture, 
                 if (err) {
                     console.log("USER SCHON VORHANDEN!");
                     console.log(err);
-                      var check = client.query(`SELECT email FROM moodledatauser WHERE userid = '24262'`, (err, data){
+                      var check = client.query(`SELECT email FROM moodledatauser WHERE userid = '24262'`), (err, data){
                         console.log("DATA TEST VON EMAILS AUSGEBEN" + check);
                       });
                 }
