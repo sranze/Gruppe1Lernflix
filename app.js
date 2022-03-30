7859726cef0c9100413c90b7a71763db5bbcfebd
OkanDev <okan.biyikli@hotmail.de>
2022-03-31 01:58:17 +0200

    Fix default Chat message revert

--- app.js
+++ app.js
@@ -161,9 +161,9 @@ io.on('connection', (socket) => {
     (async () => {
       const roomInformation = await loadRooms(moodleRoom);
       //räume werden nicht geladen wenn korrigiert.
-//                  io.to(socket.id).emit('welcome', welcomeMessage('System', `Willkommen zu Lernflix! Am oberen Bildschirmrand kannst Du Räume finden, denen Du beitreten kannst. Klicke einfach auf einen.\nWähle danach das Video aus.` + socket.client.conn.server.clientsCount, roomInformation));
-
-      io.to(socket.id).emit('welcome', welcomeMessage('System', `Willkommen zu Lernflix! Am oberen Bildschirmrand kannst Du Räume finden, denen Du beitreten kannst. Klicke einfach auf einen.\nWähle danach das Video aus.`, roomInformation));
+                  io.to(socket.id).emit('welcome', welcomeMessage('System', `Willkommen zu Lernflix! Am oberen Bildschirmrand kannst Du Räume finden, denen Du beitreten kannst. Klicke einfach auf einen.\nWähle danach das Video aus.` + socket.client.conn.server.clientsCount, roomInformation));
+//
+//      io.to(socket.id).emit('welcome', welcomeMessage('System', `Willkommen zu Lernflix! Am oberen Bildschirmrand kannst Du Räume finden, denen Du beitreten kannst. Klicke einfach auf einen.\nWähle danach das Video aus.`, roomInformation));
       io.to(socket.id).emit('welcome', welcomeMessage('System', `Nutzer:innen gerade online: ` + socket.client.conn.server.clientsCount, roomInformation));
 
     });
